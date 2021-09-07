43c125ae84156dbe2951b91a36f88b473067365e
ethanknights <ethanknights@hotmail.co.uk>
2021-09-07 17:41:22 +0100

    ff

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # hitchcockMovieCoding
 
-<h1> Convert Movie To Frames </h1>
+<h1> 1. Convert Movie To Frames </h1>
 $ cd utils/convertMovieToFrames
 $ python run_extractMovieFrames.py
   #Note use anaconda: /Users/ethanknights/anaconda3/bin/python
--- utils/convertMovieToFrames/run_extractMovieFrames.py
+++ utils/convertMovieToFrames/run_extractMovieFrames.py
@@ -2,8 +2,12 @@ import cv2
 import time
 import os
 
-os.chdir('/Users/ethanknights/imaging/projects/hitchcock_movie/utils/convertMovieToFrames')
 fN_movie = '/Users/ethanknights/ownCloud/share/Cam-CAN/movieStimuli_Hitchcock/bangbangyouredead_v2 corrected.mov'
+oN = os.path.join(os.path.dirname(os.path.dirname(os.getcwd())),'data','rawMovieFrames')
+print(oN)
+os.mkdir(oN)
 
 from extractMovieFrames import video_to_frames
-video_to_frames(fN_movie, 'test')
+video_to_frames(fN_movie, oN)
+
+os.remove('__pycache__')
