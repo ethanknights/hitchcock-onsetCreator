8109063e85a67bf5b3b4c0d0c3aa8f7076c01742
ethanknights <ethanknights@hotmail.co.uk>
2021-09-08 13:44:46 +0100

    update 01

--- 01_convertMovieToFrames/run_extractMovieFrames.py
+++ 01_convertMovieToFrames/run_extractMovieFrames.py
@@ -3,11 +3,11 @@ import time
 import os
 
 fN_movie = '/Users/ethanknights/ownCloud/share/Cam-CAN/movieStimuli_Hitchcock/bangbangyouredead_v2 corrected.mov'
-oN = os.path.join(os.path.dirname(os.path.dirname(os.getcwd())),'data','rawMovieFrames')
-print(oN)
-os.mkdir(oN)
+oN = 'data'
+if not os.path.exists(oN):
+    os.makedirs(oN)
 
 from extractMovieFrames import video_to_frames
 video_to_frames(fN_movie, oN)
 
-os.remove('__pycache__')
+#os.remove('__pycache__')
